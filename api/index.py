adb6482a317fbe52ed8f3b8fdb413273328e6c7f
Flaviozettel <flavio.zettel@students.fhnw.ch>
2025-01-01 18:33:15 +0100

    Anpassung

--- api/index.py
+++ api/index.py
@@ -19,7 +19,7 @@ allow_headers=["*"],
 with open("./data/meteodaten_2023_daily.json", "r") as data:
     meteodaten = json.load(data)
 
-@app.get("/Meteodaten/Standortnamen")
+@app.get("/api/py/Meteodaten/Standortnamen")
 async def getStandortnamen():
 
     optionenStandort=list(set(i["Standortname"] for i in meteodaten))
@@ -29,14 +29,14 @@ async def getStandortnamen():
 
 
 
-@app.get("/Altair/Säulendiagramm")
+@app.get("/api/py/Altair/Säulendiagramm")
 async def getSäulen(Standort: str = Query(...), Abfrage: str = Query(...), TSstartDate: int = Query(...), TSendDate: int = Query(...)):
     return Saeulen(Standort, Abfrage, TSstartDate, TSendDate)
 
-@app.get("/Altair/Liniendiagramm")
+@app.get("/api/py/Altair/Liniendiagramm")
 async def getLinien(Standort: str = Query(...), Abfrage: str = Query(...), TSstartDate: int = Query(...), TSendDate: int = Query(...)):
     return Linien(Standort, Abfrage, TSstartDate, TSendDate)
 
-@app.get("/Altair/Punktediagramm")
+@app.get("/api/py/Altair/Punktediagramm")
 async def getLinien(Standort: str = Query(...), Abfrage: str = Query(...), TSstartDate: int = Query(...), TSendDate: int = Query(...)):
     return Punkte(Standort, Abfrage, TSstartDate, TSendDate)
